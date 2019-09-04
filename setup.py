cd31fc88b221d7371ff93e5467016ed153389902
Andrew Magee <andrew.magee@biarri.com>
2019-09-04 15:02:14 +1000

    bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="webdriver_components",
-    version="0.10.0",
+    version="0.11.0",
     author="Andrew Magee",
     author_email="amagee@gmail.com",
     description="Webdriver_components",
